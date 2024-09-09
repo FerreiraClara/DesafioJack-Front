edf87891ee51c0112d723bee578c8b22ab8c2b53
Clara Rocha Ferreira <115419127+FerreiraClara@users.noreply.github.com>
2024-09-09 16:37:09 -0300

    End settings
    End settings

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -8,12 +8,16 @@ import '../App.css'
 function Login() {
   const [email, setEmail] = useState('')
   const [password, setPassword] = useState('')
+  const [showError, setShowError] = useState(false)
   const navigate = useNavigate()
   async function submitForm(){
-    const response = await Api({email, password},'POST','/login')
+    const response = await Api({email, password},'POST','/login').catch((err)=>{
+      setShowError(true)
+    })
     console.log(response)
     if(response?.success) navigate('/')
     if(response?.response?.token)localStorage.setItem('token',response?.response?.token)
+    if(!response?.success) setShowError(true)
   }
 
   return (
@@ -44,8 +48,9 @@ function Login() {
               setPassword(e.target.value)
             }}
           />
-          <a underline="none" style={{ fontSize: 12, fontFamily: 'Arial' }}> <Link to='/register' style={{ textDecoration: 'none', color: '#000' }}> Ainda não é cadastrado? Cadastre-se aqui </Link> </a>
+          <a underline="none" style={{ fontSize: 12, fontFamily: 'Arial' }}> <Link to='/register' style={{ textDecoration: 'none', color: '#000' }}> Ainda não é cadastrado? <p style={{color:'#e73555c9'}}>Cadastre-se aqui</p> </Link> </a>
           <Button id='access-login-button' variant="contained" style={{ display: 'grid', marginBottom: 10, backgroundColor: '#ff3c5f', fontSize: 14, width: '100%' }} onClick={submitForm}> Acessar </Button>
+          <div style={{width:"380px"}}><p style={{display:!showError?"none":"block", color:"#c33737",fontSize: 12,fontFamily: 'Arial'}}>Ocorreu um erro ao logar, verifique sua senha e tente novamente mais tarde</p></div>
         </CardContent>
       </Card>
     </>
